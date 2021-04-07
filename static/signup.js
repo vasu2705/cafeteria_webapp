7499197f4f9f8d015ea989f8d5c97ff86a75b38e
vasudev kanungo <vasukanungo4040@gmail.com>
2021-04-07 23:10:35 +0530

    add js

--- static/signup.js
+++ static/signup.js
@@ -1,4 +1,17 @@
-var result = document.getElementById('random_String'); 
-	function random_String_Generator() { 
-		result.innerHTML = Math.random().toString(36).slice(2); 
-	} 
+// var result = document.getElementById('random_String');
+// 	function random_String_Generator() {
+// 		result.innerHTML = Math.random().toString(36).slice(2);
+// 	}
+
+var form = document.getElementById("form");
+
+form.addEventListener("mousemove", (e) => {
+  var x = (window.innerWidth / 2 - e.pageX) / 12;
+  var y = (window.innerHeight / 2 - e.pageY) / 12;
+
+  form.style.transform = "rotateX(" + x + "deg)rotateY(" + y + "deg)";
+});
+
+form.addEventListener("mouseleave", function () {
+  form.style.transform = "rotateX(0deg) rotateY(0deg)";
+});
